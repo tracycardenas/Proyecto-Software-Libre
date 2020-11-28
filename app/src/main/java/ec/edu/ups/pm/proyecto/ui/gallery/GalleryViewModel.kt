ab1dfbf440b0fba290941d4d6db68dd9c4fde4cb
tracycardenas <65791787+tracycardenas@users.noreply.github.com>
2020-11-27 23:01:43 -0500

    Inicio del proyecto

---
+++ app/src/main/java/ec/edu/ups/pm/proyecto/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package ec.edu.ups.pm.proyecto.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/ec/edu/ups/pm/proyecto/ui/slideshow/SlideshowViewModel.kt
@@ -0,0 +1,13 @@
+package ec.edu.ups.pm.proyecto.ui.slideshow
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SlideshowViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is slideshow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
