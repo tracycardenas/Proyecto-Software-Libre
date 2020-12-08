c61435fc165bb8a7eb8b288ab5820c64ecf5047b
tracycardenas <65791787+tracycardenas@users.noreply.github.com>
2020-12-08 03:04:11 -0500

    Creación de íconos

---
+++ app/src/main/java/ec/edu/ups/pm/proyecto/InicioActivity.kt
@@ -0,0 +1,12 @@
+package ec.edu.ups.pm.proyecto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InicioActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inicio)
+    }
+}
